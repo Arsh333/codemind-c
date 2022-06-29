f59c6ac9f6ebf83a57f6bda2e36b191f74295bda
Arsh333 <97731955+Arsh333@users.noreply.github.com>
2022-06-29 21:40:21 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n>0)     {         printf("Positive Number");
        }
        else if(n<0)
        {
        printf("Negative Number");
        }
        else if(n=0)
        {
            printf("0 is neither positive nor negative");
            
        }
        else
        {
            printf("your input is not in our lmit");
            
        }
        return 0;
        
    }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("Positive Number");
+        
+    }
+    else if(n<0)
+    {
+    printf("Negative Number");
+    }
+    else if(n=0)
+    {
+        printf("0 is neither positive nor negative");
+        
+    }
+    else
+    {
+        printf("your input is not in our lmit");
+        
+    }
+    return 0;
+    
+}
